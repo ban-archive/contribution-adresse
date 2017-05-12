b204ac4e5ccc6599e79063a12d9c7b70c9cb794d
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-05-12 10:26:10 +0200

    Move front dist directory

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-app.use('/dist', express.static(__dirname + '/front/dist'))
+app.use('/dist', express.static(__dirname + '/dist'))
 app.use(express.static(__dirname + '/front/public'))
 
 app.get('*', function (req, res) {
