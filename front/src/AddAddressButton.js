be476914c81953ad3659b9c9230d86bcb343914c
tmerlier <tmerlier@student.42.fr>
2017-05-23 16:50:46 +0200

    add accuracy and speed value

--- front/src/AddAddressButton.js
+++ front/src/AddAddressButton.js
@@ -1,7 +1,7 @@
 const { h } = preact
 
 const AddAddressButton = ({ action }) => (
-<div onClick={action}>
+<div class="circle" onClick={action}>
   <div class="AddAddressButton">
   <img src="addHome.svg"/>
   </div>
