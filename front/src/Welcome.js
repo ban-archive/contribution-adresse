c17e52d0f5ab0688a934e4c75340c9e1a7dd7c0a
tmerlier <tmerlier@student.42.fr>
2017-05-31 15:45:09 +0200

    Add welcome page

---
+++ front/src/Welcome.js
@@ -0,0 +1,19 @@
+class Welcome extends Component {
+  render() {
+    const { skip } = this.props
+
+    return (
+      <div class="Welcome">
+        <div class="head">
+          <div><h1>C<img class="logo" src="addHome.svg"/>ntrib'Adresses</h1></div>
+          <div class="legend">Outil de contribution citoyenne aux référentiels d'adresses</div>
+          <p>Ras-le-bol que personne ne trouve votre adresse ?</p>
+        </div>
+        <div class="slogan">
+          <div><img class="gps" src="gps.png" /></div>
+          <div><button onClick={skip}>Je veux contribuer !</button></div>
+        </div>
+      </div>
+    )
+  }
+}
