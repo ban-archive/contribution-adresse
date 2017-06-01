21e252fd1814bde610a784ef148340f6b5abf2be
tmerlier <tmerlier@student.42.fr>
2017-06-01 17:40:29 +0200

    Add alt

--- front/src/Welcome.js
+++ front/src/Welcome.js
@@ -5,12 +5,12 @@ class Welcome extends Component {
     return (
       <div class="Welcome">
         <div class="head">
-          <div><h1>C<img class="logo" src="addHome.svg"/>ntrib'Adresses</h1></div>
+          <div><h1>C<img class="logo" alt="o" src="addHome.svg"/>ntrib'Adresses</h1></div>
           <div class="legend">Outil de contribution citoyenne aux référentiels d'adresses</div>
           <p>Ras-le-bol que personne ne trouve votre adresse ?</p>
         </div>
         <div class="slogan">
-          <div><img class="gps" src="gps.svg" /></div>
+          <div><img class="gps" alt="illustration gps" src="gps.svg" /></div>
           <div><button onClick={skip}>Je veux contribuer !</button></div>
         </div>
       </div>
