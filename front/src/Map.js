24a7866158211b8d3344c7f60deaecd566d0bb2d
tmerlier <tmerlier@student.42.fr>
2017-06-26 10:28:55 +0200

    locator display

--- front/src/Map.js
+++ front/src/Map.js
@@ -6,12 +6,11 @@ import Locator from './Locator'
 export default class Map extends Component {
   render() {
     const { user, addresses, selectedAddress, coords, fullscreen, displayAddress, closeMenu } = this.props
-    if (this.leafletMap) this.leafletMap.forceUpdate()
 
     return (
       <div>
         <LeafletMap ref={ref => this.leafletMap = ref} user={user} addresses={addresses} selectedAddress={selectedAddress} displayAddress={displayAddress} onCloseMenu={closeMenu} coords={coords} fullscreen={fullscreen} />
-        {!selectedAddress ? <Locator accuracy={coords.accuracy} fullscreen={fullscreen} /> : null}
+        <Locator accuracy={coords.accuracy} display={fullscreen} />
       </div>
     )
   }
